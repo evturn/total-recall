2fa44a30d0ed3ee15f0e0170d6d49baf00f36853
Evan Turner <evturn@gmail.com>
2016-06-06 14:40:01 -0400

    Create cardsStore

---
+++ src/stores/CardsStore.js
@@ -0,0 +1,69 @@
+import { AsyncStorage } from 'react-native'
+import Reflux from 'reflux'
+import Card from '../data/Card'
+import { CardActions } from '../actions'
+
+import find from 'lodash.find'
+
+const CARD_KEY = 'total-recall-cards'
+
+const cardsStore = Reflux.createStore({
+  init() {
+    this.loadCards().done()
+    this.listenTo(CardActions.createCard,     this.createCard)
+    this.listenTo(CardActions.deleteAllCards, this.deleteAllCards)
+    this.listenTo(CardActions.editCard,       this.editCard)
+    this.cards = []
+    this.emit()
+  }
+
+  async loadCards() {
+    try {
+      const val = await AsyncStorage.getItem(CARD_KEY)
+
+      if (val !== null) {
+        this.cards = JSON.parse(val).map(cardObj => Card.fromObject(cardObj))
+        this.emit()
+      } else {
+        console.info(`${CARD_KEY} not found on disk.`);
+      }
+    } catch (e) {
+      console.error('AsyncStorage error:', e.message)
+    }
+  }
+
+  async writeCards() {
+    try {
+      await AsyncStorage.setItem(CARD_KEY, JSON.stringify(this.cards))
+    } catch (e) {
+      console.error('AsyncStorage error:', e.message)
+    }
+  }
+
+  deleteAllCards() {
+    this.cards = []
+    this.emit()
+  }
+
+  editCard(newCard) {
+    const match = find(
+      this.cards,
+      card => card.id === newCard.id
+    )
+
+    match.setFromObject(newCard)
+    this.emit()
+  }
+
+  createCard(front, back, deckID) {
+    this.cards.push(new Card(front, back, deckID))
+    this.emit()
+  }
+
+  emit() {
+    this.writeCards().done()
+    this.trigger(this.cards)
+  }
+})
+
+export default cardsStore
