a2b7d6d902f3a234628ed3d7122bd21c8c2358fe
Evan Turner <evturn@gmail.com>
2016-06-06 13:32:42 -0400

    Initialize repo

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native'
+import Recall from './src/components/Recall'
+
+AppRegistry.registerComponent('Recall', _ => Recall)
---
+++ src/data/Deck.js
@@ -0,0 +1,30 @@
+import md5 from 'md5'
+
+class Deck {
+  constructor(name) {
+    this.name = name
+    this.totalCards = 0
+    this.dueCards = 0
+    this.id = md5(name)
+  }
+
+  setFromObject(obj) {
+    this.name = obj.name
+    this.totalCards = obj.totalCards
+    this.dueCards = obj.dueCards
+    this.id = obj.id
+  }
+
+  resetCounts() {
+    this.totalCards = 0
+    this.dueCards = 0
+  }
+
+  static from Object(obj) {
+    const deck = new Deck(obj.name)
+    deck.setFromObject(obj)
+    return deck
+  }
+}
+
+export default Deck
